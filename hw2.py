c4fa7be4d914fae1097dead48a6cedd44730dfea
Alvin Wang <alvinwang@169-231-113-158.wireless.ucsb.edu>
2024-05-05 22:13:24 -0700

    new

--- hw2.py
+++ hw2.py
@@ -183,7 +183,7 @@ model = Binary_Classifier(train_data, train_target)
 
 # Logistic Regression
 logistic_start = time.time()
-model.logistic_training([10**-10, 10], [10e-10, 1e10], 120, 0.01)
+model.logistic_training([10**-10, 10], [10e-10, 1e10], 140, 0.01)
 logistic_end = time.time()
 # SVM
 svm_start = time.time()
